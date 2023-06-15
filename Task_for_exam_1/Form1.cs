b8dbb844b99ceb13a0db0d7180b6320708f50db6
MrAladawee <91622707+MrAladawee@users.noreply.github.com>
2023-06-15 11:38:53 +0300

    Add files via upload

---
+++ Task_for_exam_1/Form1.cs
@@ -0,0 +1,38 @@
+namespace Task_for_exam_1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_Click(object sender, EventArgs e)
+        {
+
+            // Bounds
+            var a = Convert.ToDouble(leftBound.Text);
+            var b = Convert.ToDouble(rightBound.Text);
+
+            Function function = new Function(a, b);
+
+            Consumer consumer = new Consumer();
+
+            Producer p1 = new Producer(0);
+            Producer p2 = new Producer(1);
+            Producer p3 = new Producer(2);
+
+
+            var count = 3;
+
+            var step = (b-a) / count;
+
+            p1.Start(function, a, a + step);
+            p2.Start(function, a + step, a + 2*step);
+            p3.Start(function, a + 2*step, b);
+            
+            consumer.Start(label1);
+
+        }
+    }
+}
